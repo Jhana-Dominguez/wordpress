3e5985acf39d39bc547bcf2dd571b8533c0e977f
unknown <joanamarie.dominguez@cvsu.edu.ph>
2021-05-05 16:30:36 +0800

    first backup

---
+++ wp-content/themes/blank/about-template.php
@@ -0,0 +1,21 @@
+<?php
+/* Template Name: About */
+get_header();
+?>
+<div>
+
+	<div class="row">
+		<div class="col-2">
+			<h1>La Bañana</h1>
+
+			<b><center><p>
+•	La Bañana offers various designs that woman would love to wear. Every type of clothes is available and designed based on every woman’s taste.</p><p>
+•	In just one click, come and add to cart your favorites.
+</p><p>
+•	Find the outfit suited for you
+</p></center></b>
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?> 
---
+++ wp-content/themes/blank/contact-template.php
@@ -0,0 +1,18 @@
+<?php
+/* Template Name: Contact */
+get_header();
+?>
+<div>
+
+<div class="contact-section">
+	<h1>CONTACT US</h1>
+<b><p>	Get in touch with us</p>
+<p>•	Mailing adress: 123 nowhere to be found state of the nation</p>
+<p>•	Email adress: Labañana@gmail.com</p>
+<p>•	Facebook page: La bañana</p>
+<p>•	Instagram: @LaBañanaCL</p>
+<p>•	YouTube: @LaBañanaCL</p>
+</b>
+</div>
+
+<?php get_footer(); ?> 
